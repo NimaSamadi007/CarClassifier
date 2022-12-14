97e2f3a237a70e948d205592dbaa616d189df47d
Nima Samadi <nbr.smd@gmail.com>
2022-12-14 20:52:26 +0330

    Create source codes - Add CMakeLists

---
+++ include/VehicleDetection.hpp
@@ -0,0 +1,11 @@
+#ifndef _VEHICLE_DETECTION_HPP_
+#define _VEHICLE_DETECTION_HPP_
+
+class VehicleDetection{
+private:
+
+public:
+    VehicleDetection();
+};
+
+#endif //_VEHICLE_DETECTION_HPP_
---
+++ src/VehicleDetection.cpp
@@ -0,0 +1,1 @@
+#include "VehicleDetection.hpp"
